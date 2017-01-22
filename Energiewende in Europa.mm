7627924f04b769c36dda560c03c11c165be6454f
PB-Coaching <pbcoaching@rppr.de>
2017-01-22 22:02:42 +0100

    Erstes Mal alle Dateien hochladen

---
+++ Energiewende in Europa.mm
@@ -0,0 +1,22 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#c5e1e7" CREATED="1453378529212" ID="ID_538063386" MODIFIED="1453382532078" TEXT="Energiewende in Europa">
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node BACKGROUND_COLOR="#f0eee4" CREATED="1453378626453" ID="ID_1670579238" MODIFIED="1453382580687" POSITION="right" TEXT="Wir wirkt sich die Energiewende auf die Nachfrage nach Erdgas aus?">
+<font ITALIC="true" NAME="SansSerif" SIZE="14"/>
+</node>
+<node BACKGROUND_COLOR="#f0eee4" CREATED="1453381962020" ID="ID_1661513454" MODIFIED="1453382591823" POSITION="right" TEXT="Wie unterscheiden sich mittelfristige &amp; langfristige Prognosen zum Erdgasbedarf?">
+<font ITALIC="true" NAME="SansSerif" SIZE="14"/>
+<node CREATED="1453382037827" ID="ID_1651454009" MODIFIED="1453382067164" TEXT="mittelfristig d&#xfc;rfte der Verbrauch steigen, da auf CO&#xb2;-arme Brennstoffe umgestellt wird"/>
+<node CREATED="1453382074771" ID="ID_1229003837" MODIFIED="1453382104362" TEXT="langfristig d&#xfc;rften erneuerbare Energien fossile Brennstoffe praktisch vollst&#xe4;ndig abl&#xf6;sen"/>
+<node CREATED="1453382584247" ID="ID_1866808411" MODIFIED="1453382584247" TEXT=""/>
+</node>
+<node BACKGROUND_COLOR="#f0eee4" CREATED="1453382440729" ID="ID_1139721808" MODIFIED="1453382572823" POSITION="left" TEXT="Welche &#xdc;berlegung zur Versorgungssicherheit spielen bei der Energiewende eine Rolle?">
+<font ITALIC="true" NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1453382483344" ID="ID_1389461468" MODIFIED="1453382499183" POSITION="left" TEXT="Energiepolitik in Europa">
+<node BACKGROUND_COLOR="#f0eee4" CREATED="1453383088029" ID="ID_192543839" MODIFIED="1453383122516" TEXT="Welche Mitgliedsl&#xe4;nder und ihre Energiepolitik sind relevant f&#xfc;r die Untersuchung?"/>
+<node CREATED="1453382503001" ID="ID_553728273" MODIFIED="1453382517896" TEXT="Darstellung der unterschiedlichen Ans&#xe4;tze"/>
+</node>
+</node>
+</map>
